62363f6a157f2f019586b45d37c95c1d624e6ff6
mdo <4ryo2-meron-math@ezweb.ne.jp>
2020-12-17 00:26:46 +0900

    [rule3] add Ball class

---
+++ src/Ball.cpp
@@ -0,0 +1,35 @@
+#include "Ball.hpp"
+
+void Ball::move() {
+    m_ball.moveBy(m_ballVelocity * Scene::DeltaTime());
+}
+
+void Ball::rebound(const Rect& paddle) {
+    // 天井にぶつかったらはね返る
+    if (m_ball.y < 0 && m_ballVelocity.y < 0) {
+        m_ballVelocity.y *= -1;
+    }
+
+    // 左右の壁にぶつかったらはね返る
+    if ((m_ball.x < 0 && m_ballVelocity.x < 0) || (Scene::Width() < m_ball.x && m_ballVelocity.x > 0)) {
+        m_ballVelocity.x *= -1;
+    }
+
+    // パドルにあたったらはね返る
+    if (m_ballVelocity.y > 0 && paddle.intersects(m_ball)) {
+        // パドルの中心からの距離に応じてはね返る向きを変える
+        m_ballVelocity = Vec2((m_ball.x - paddle.center().x) * 10, -m_ballVelocity.y).setLength(m_speed);
+    }
+}
+
+void Ball::draw() const {
+    m_ball.draw();
+}
+
+Circle Ball::getBall() const {
+    return m_ball;
+}
+
+Vec2& Ball::getBallVelocity() {
+    return m_ballVelocity;
+}
